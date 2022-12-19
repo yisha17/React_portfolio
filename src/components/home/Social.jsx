04eff535329b731f93f705d8eae36f626fdc3dd4
Yishak Wondim <it.yisak.wondim@gmail.com>
2022-12-19 16:07:25 +0300

    home and header added

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home_social">
+        <a href="_blank" className="home_social-icon">
+            <i class='uil uil-instagram'></i>
+        </a>
+        <a href="_blank" className="home_social-icon">
+            <i class='uil uil-dribbble'></i>
+        </a>
+        <a href="_blank" className="home_social-icon">
+            <i class='uil uil-github-alt'></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
